5bbd9af0610c4fb339e67e79b8a79617f484b973
QRETIO <78140256+QRETIO@users.noreply.github.com>
2021-06-26 20:09:41 +0800

    git-test

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -3,9 +3,7 @@ package com.bjpowernode.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git!");
-        System.out.println("hello git!!");
-        System.out.println("hello git!");
+     
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push");
